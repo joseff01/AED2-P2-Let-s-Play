b2612345c986f5e3839de1d69ead649435879201
Ignacio Vargas <ignaevc@gmail.com>
2021-05-24 00:39:19 -0600

    Defined node generic node class for linked list

---
+++ LinkedList/Node.h
@@ -0,0 +1,22 @@
+#ifndef PERSONAL_LINKED_LIST_NODE_H
+#define PERSONAL_LINKED_LIST_NODE_H
+
+template <typename T>
+class Node
+{
+public:
+    //! Holds reference to next node
+    Node<T> *next;
+
+    //! Holds template type value of node
+    T value;
+
+    /*!
+     * \brief Construct a new Node object, the value for it must be supplied when construfcted
+     * 
+     * \param value the value being assigned to the node
+     */
+    Node(const T value) : next(nullptr), value(val) {}
+};
+
+#endif
