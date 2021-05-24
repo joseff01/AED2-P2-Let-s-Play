4c6e76b6f01f1cd1ad78400f8def7cd56cf11b15
Jose Retana <joseantonioretanacorrales@gmail.com>
2021-05-24 03:19:38 -0600

    C# Client & Backtracking debugging
    Se crean los inicios del cliente de C#
    Se debuggea el algoritmo de backtracking, el cual presentaba múltiples problemas de lógica para que funcione más efectivamente

--- BPGame/BPServer/BacktrackingAlgorithm.cpp
+++ BPGame/BPServer/BacktrackingAlgorithm.cpp
@@ -1,9 +1,16 @@
 #include "BacktrackingAlgorithm.h"
 #include <iostream>
-#include <cstring>
 
-void* BacktrackingAlgorithm::findPath(int elementsMatrix[7][9]){
+void BacktrackingAlgorithm::basePathMatrixAssignation(){
+    for (int i = 0; i < 7; i++){
+        basePathMatrix[i] = new int[9];
+    }
+}
+
+int** BacktrackingAlgorithm::findPath(int elementsMatrix[7][9]){
     int initialPoint[2];
+    finalPoint[0] = 0;
+    finalPoint[1] = 0;
     counterPath = 1;
     //Search for first element position AND copy the received matrix to the attribute in BacktrackingAlgorithm
     for (int i = 0; i < 7; i++){
@@ -13,6 +20,9 @@ void* BacktrackingAlgorithm::findPath(int elementsMatrix[7][9]){
             if (elementsMatrix[i][j] == 3){
                 initialPoint[0] = i;
                 initialPoint[1] = j;
+            } if (elementsMatrix[i][j] == 2){
+                finalPoint[0] = i;
+                finalPoint[1] = j;
             }
         }
     }
@@ -24,109 +34,235 @@ bool BacktrackingAlgorithm::recursivePathing(const int initialPoint[2]) {
     int row = initialPoint[0];
     int column = initialPoint[1];
     basePathMatrix[row][column] = counterPath;
+
     //west movement
-    if (((row < 7)&&(0 <= row)) && ((column - 1 < 9)&&(0 <= column - 1))){
-        if ((receivedElementsMatrix[row][column - 1 ] != 1) && (basePathMatrix[row][column - 1] == 0)) {
+    if (((row < 7)&&(0 <= row)) && ((column - 1 < 9)&&(0 <= column - 1))) {
+        if ((receivedElementsMatrix[row][column - 1] != 1) && (basePathMatrix[row][column - 1] == 0)) {
+            std::cout << "west" << std::endl;
             if (receivedElementsMatrix[row][column - 1] == 2) {
+                std::cout << "W SUCCESS" << std::endl;
                 basePathMatrix[row][column - 1] = counterPath + 1;
                 return true;
             } else {
                 counterPath++;
                 int newInitialPoint[2] = {row, column - 1};
-                if (recursivePathing(newInitialPoint)){
+                if (recursivePathing(newInitialPoint)) {
                     return true;
                 }
             }
         }
-    //northwest movement
-    } else if (((row - 1 < 7)&&(0 <= row - 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
-        if ((receivedElementsMatrix[row - 1][column - 1] != 1) && (basePathMatrix[row - 1][column - 1] == 0)) {
-            if (receivedElementsMatrix[row - 1][column - 1] == 2) {
-                basePathMatrix[row - 1][column - 1] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row - 1, column - 1};
-                if (recursivePathing(newInitialPoint)){
+    }if ((finalPoint[0] - initialPoint[0]) > 0){
+        //southwest movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
+            if ((receivedElementsMatrix[row + 1][column - 1] != 1) && (basePathMatrix[row + 1][column - 1] == 0)) {
+                std::cout << "southwest" << std::endl;
+                if (receivedElementsMatrix[row + 1][column - 1] == 2) {
+                    std::cout << "SW SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column - 1] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column - 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
                 }
             }
         }
-    //southeast movement
-    } else if (((row + 1 < 7)&&(0 <= row + 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
-        if ((receivedElementsMatrix[row + 1][column - 1] != 1) && (basePathMatrix[row + 1][column - 1] == 0)) {
-            if (receivedElementsMatrix[row + 1][column - 1] == 2) {
-                basePathMatrix[row + 1][column - 1] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row + 1, column - 1};
-                if (recursivePathing(newInitialPoint)){
+        //south movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column < 9)&&(0 <= column))) {
+            if ((receivedElementsMatrix[row + 1][column] != 1) && (basePathMatrix[row + 1][column] == 0)) {
+                std::cout << "south" << std::endl;
+                if (receivedElementsMatrix[row + 1][column] == 2) {
+                    std::cout << "S SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column};
+                    if (recursivePathing(newInitialPoint)) {
+                        return true;
+                    }
                 }
             }
         }
-    //north movement
-    } else if (((row - 1 < 7)&&(0 <= row - 1)) && ((column < 9)&&(0 <= column))) {
-        if ((receivedElementsMatrix[row - 1][column] != 1) && (basePathMatrix[row - 1][column] == 0)) {
-            if (receivedElementsMatrix[row - 1][column] == 2) {
-                basePathMatrix[row - 1][column] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row - 1, column};
-                if (recursivePathing(newInitialPoint)){
+        //northwest movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
+            if ((receivedElementsMatrix[row - 1][column - 1] != 1) && (basePathMatrix[row - 1][column - 1] == 0)) {
+                std::cout << "northwest" << std::endl;
+                if (receivedElementsMatrix[row - 1][column - 1] == 2) {
+                    std::cout << "NW SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column - 1] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column - 1};
+                    if (recursivePathing(newInitialPoint)) {
+                        return true;
+                    }
                 }
             }
         }
-    //south movement
-    } else if (((row + 1 < 7)&&(0 <= row + 1)) && ((column < 9)&&(0 <= column))) {
-        if ((receivedElementsMatrix[row + 1][column] != 1) &&
-            (basePathMatrix[row + 1][column] == 0)) {
-            if (receivedElementsMatrix[row + 1][column] == 2) {
-                basePathMatrix[row + 1][column] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row + 1, column};
-                if (recursivePathing(newInitialPoint)){
+        //north movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column < 9)&&(0 <= column))) {
+            if ((receivedElementsMatrix[row - 1][column] != 1) && (basePathMatrix[row - 1][column] == 0)) {
+                std::cout << "north" << std::endl;
+                if (receivedElementsMatrix[row - 1][column] == 2) {
+                    std::cout << "N SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
                 }
             }
         }
-    // northeast movement
-    } else if (((row - 1 < 7)&&(0 <= row - 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
-        if ((receivedElementsMatrix[row - 1][column + 1] != 1) && (basePathMatrix[row - 1][column + 1] == 0)) {
-            if (receivedElementsMatrix[row - 1][column + 1] == 2) {
-                basePathMatrix[row - 1][column + 1] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row - 1, column + 1};
-                if (recursivePathing(newInitialPoint)){
+        //southeast movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
+            if ((receivedElementsMatrix[row + 1][column + 1] != 1) && (basePathMatrix[row + 1][column + 1] == 0)) {
+                std::cout << "southeast" << std::endl;
+                if (receivedElementsMatrix[row + 1][column + 1] == 2) {
+                    std::cout << "SE SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column + 1] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column + 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
                 }
             }
         }
-    //southeast movement
-    } else if (((row + 1 < 7)&&(0 <= row + 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
-        if ((receivedElementsMatrix[row + 1][column + 1] != 1) && (basePathMatrix[row + 1][column + 1] == 0)) {
-            if (receivedElementsMatrix[row + 1][column + 1] == 2) {
-                basePathMatrix[row + 1][column + 1] = counterPath + 1;
-                return true;
-            } else {
-                counterPath++;
-                int newInitialPoint[2] = {row + 1, column + 1};
-                if (recursivePathing(newInitialPoint)){
+        // northeast movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
+            if ((receivedElementsMatrix[row - 1][column + 1] != 1) && (basePathMatrix[row - 1][column + 1] == 0)) {
+                std::cout << "northeast" << std::endl;
+                if (receivedElementsMatrix[row - 1][column + 1] == 2) {
+                    std::cout << "NE SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column + 1] = counterPath + 1;
                     return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column + 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
+                }
+            }
+        }
+    } else {
+        //northwest movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
+            if ((receivedElementsMatrix[row - 1][column - 1] != 1) && (basePathMatrix[row - 1][column - 1] == 0)) {
+                std::cout << "northwest" << std::endl;
+                if (receivedElementsMatrix[row - 1][column - 1] == 2) {
+                    std::cout << "NW SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column - 1] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column - 1};
+                    if (recursivePathing(newInitialPoint)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        //north movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column < 9)&&(0 <= column))) {
+            if ((receivedElementsMatrix[row - 1][column] != 1) && (basePathMatrix[row - 1][column] == 0)) {
+                std::cout << "north" << std::endl;
+                if (receivedElementsMatrix[row - 1][column] == 2) {
+                    std::cout << "N SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
+                }
+            }
+        }
+        //southwest movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column - 1 < 9)&&(0 <= column - 1))) {
+            if ((receivedElementsMatrix[row + 1][column - 1] != 1) && (basePathMatrix[row + 1][column - 1] == 0)) {
+                std::cout << "southwest" << std::endl;
+                if (receivedElementsMatrix[row + 1][column - 1] == 2) {
+                    std::cout << "SW SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column - 1] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column - 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
+                }
+            }
+        }
+        //south movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column < 9)&&(0 <= column))) {
+            if ((receivedElementsMatrix[row + 1][column] != 1) && (basePathMatrix[row + 1][column] == 0)) {
+                std::cout << "south" << std::endl;
+                if (receivedElementsMatrix[row + 1][column] == 2) {
+                    std::cout << "S SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column};
+                    if (recursivePathing(newInitialPoint)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        // northeast movement
+        if (((row - 1 < 7)&&(0 <= row - 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
+            if ((receivedElementsMatrix[row - 1][column + 1] != 1) && (basePathMatrix[row - 1][column + 1] == 0)) {
+                std::cout << "northeast" << std::endl;
+                if (receivedElementsMatrix[row - 1][column + 1] == 2) {
+                    std::cout << "NE SUCCESS" << std::endl;
+                    basePathMatrix[row - 1][column + 1] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row - 1, column + 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
+                }
+            }
+        }
+        //southeast movement
+        if (((row + 1 < 7)&&(0 <= row + 1)) && ((column + 1 < 9)&&(0 <= column + 1))) {
+            if ((receivedElementsMatrix[row + 1][column + 1] != 1) && (basePathMatrix[row + 1][column + 1] == 0)) {
+                std::cout << "southeast" << std::endl;
+                if (receivedElementsMatrix[row + 1][column + 1] == 2) {
+                    std::cout << "SE SUCCESS" << std::endl;
+                    basePathMatrix[row + 1][column + 1] = counterPath + 1;
+                    return true;
+                } else {
+                    counterPath++;
+                    int newInitialPoint[2] = {row + 1, column + 1};
+                    if (recursivePathing(newInitialPoint)){
+                        return true;
+                    }
                 }
             }
         }
     //east movement
-    } else if (((row < 7)&&(0 <= row)) && ((column + 1 < 9)&&(0 <= column + 1))) {
+    }if (((row < 7)&&(0 <= row)) && ((column + 1 < 9)&&(0 <= column + 1))) {
         if ((receivedElementsMatrix[row][column + 1] != 1) && (basePathMatrix[row][column + 1] == 0)) {
+            std::cout << "east" << std::endl;
             if (receivedElementsMatrix[row][column + 1] == 2) {
+                std::cout << "E SUCCESS" << std::endl;
                 basePathMatrix[row][column + 1] = counterPath + 1;
                 return true;
             } else {
@@ -137,12 +273,14 @@ bool BacktrackingAlgorithm::recursivePathing(const int initialPoint[2]) {
                 }
             }
         }
-    } else {
-        basePathMatrix[row][column] = 0;
-        return false;
     }
+    std::cout << "failure, going back" << std::endl;
+    counterPath--;
+    basePathMatrix[row][column] = 0;
     return false;
 }
 
-BacktrackingAlgorithm::BacktrackingAlgorithm() = default;
+BacktrackingAlgorithm::BacktrackingAlgorithm() {
+    basePathMatrixAssignation();
+}
 
--- BPGame/BPServer/BacktrackingAlgorithm.h
+++ BPGame/BPServer/BacktrackingAlgorithm.h
@@ -5,12 +5,14 @@
 class BacktrackingAlgorithm {
 private:
     int counterPath = 1;
+    int finalPoint[2] = {0,0};
     int receivedElementsMatrix[7][9]{};
-    int (*basePathMatrix)[9] = new int[7][9];
+    int** basePathMatrix = new int*[7];
+    void basePathMatrixAssignation();
     bool recursivePathing(const int initialPoint[2]);
 public:
     BacktrackingAlgorithm();
-    void* findPath(int elementsMatrix[7][9]);
+    int** findPath(int elementsMatrix[7][9]);
 };
 
 
