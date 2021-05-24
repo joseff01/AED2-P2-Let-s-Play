d2b00f0712ecdd474c3d9fb40021783c0bc0dfcc
Jose Retana <joseantonioretanacorrales@gmail.com>
2021-05-24 03:24:39 -0600

    More Backtracking Doxygen Documentation
    XD

--- BPGame/BPServer/BacktrackingAlgorithm.h
+++ BPGame/BPServer/BacktrackingAlgorithm.h
@@ -4,9 +4,21 @@
 
 class BacktrackingAlgorithm {
 private:
+    /**
+     * Counter for the step by step path for the findPathfunction
+     */
     int counterPath = 1;
+    /**
+     * Position of the final point of the received matrix from the client
+     */
     int finalPoint[2] = {0,0};
+    /**
+     * Matrix received from the client, contains the obstacles, start and finish.
+     */
     int receivedElementsMatrix[7][9]{};
+    /**
+     * Empty Base matrix which will be filled with the first found path when the function find path is called
+     */
     int** basePathMatrix = new int*[7];
     void basePathMatrixAssignation();
     bool recursivePathing(const int initialPoint[2]);
