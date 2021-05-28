241dab2c9908fc2f8f6f69c0cb2a98346904c212
Ignacio Vargas <ignaevc@gmail.com>
2021-05-28 12:32:54 -0600

    Changed hardcoded values of 7 and 11 to use constants defined in MapConstants.h

--- BPGame/BPServer/BacktrackingAlgorithm.h
+++ BPGame/BPServer/BacktrackingAlgorithm.h
@@ -1,6 +1,7 @@
 #ifndef BPSERVER_BACKTRACKINGALGORITHM_H
 #define BPSERVER_BACKTRACKINGALGORITHM_H
 
+#include "MapConstants.h"
 
 class BacktrackingAlgorithm {
 private:
@@ -15,16 +16,16 @@ private:
     /**
      * Matrix received from the client, contains the obstacles, start and finish.
      */
-    int receivedElementsMatrix[7][11]{};
+    int receivedElementsMatrix[MAPROWS][MAPCOLUMNS]{};
     /**
      * Empty Base matrix which will be filled with the first found path when the function find path is called
      */
-    int** basePathMatrix = new int*[7];
+    int** basePathMatrix = new int*[MAPROWS];
     void basePathMatrixAssignation();
     bool recursivePathing(const int initialPoint[2]);
 public:
     BacktrackingAlgorithm();
-    int** findPath(int elementsMatrix[7][11]);
+    int** findPath(int elementsMatrix[MAPROWS][MAPCOLUMNS]);
 };
 
 
