c696ec2e425e26f2ac3341009ceb9f556e702ec1
Ignacio Vargas <ignaevc@gmail.com>
2021-06-01 01:02:36 -0600

    tiny progress on line renderer

--- BPGame/BP-Game/Assets/Scripts/DrawPredictiveLine.cs
+++ BPGame/BP-Game/Assets/Scripts/DrawPredictiveLine.cs
@@ -26,6 +26,11 @@ public class DrawPredictiveLine : MonoBehaviour
         lineRenderer.positionCount = mapMatrix.Cast<int>().Max();
         lineRenderer.useWorldSpace = true;
 
+        // lineRenderer.material = GetComponent<Renderer>().material;
+        // lineRenderer.sortingLayerName = "Foreground";
+        // mis intentos de asignar un material y sorting layer xd
+
+
         while (drawing)
         {
             Vector3 firstPoint = new Vector3(123123, 1231231, 1231231);
@@ -35,7 +40,7 @@ public class DrawPredictiveLine : MonoBehaviour
                 {
                     if (mapMatrix[i, j] == counter)
                     {
-                        firstPoint = new Vector3(j * gridWidth / 7 + (float)tileWidth / 2f - 5.5f, i * gridHeight / 11 + (float)tileHeight / 2f - 3.5f, -10);
+                        firstPoint = new Vector3(j * gridWidth / 7 + (float)tileWidth / 2f, i * gridHeight / 11 + (float)tileHeight / 2f, 0);
                         foundNext = true;
                     }
                 }
@@ -43,7 +48,7 @@ public class DrawPredictiveLine : MonoBehaviour
 
             if (foundNext)
             {
-                lineRenderer.SetPosition(0, firstPoint);
+                lineRenderer.SetPosition(counter - 1, firstPoint);
                 counter++;
                 foundNext = false;
             }
